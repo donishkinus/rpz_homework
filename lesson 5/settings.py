e6f582e3232708b747252a3ded8b9f1f5c6be8fa
donishkinus <108665225+donishkinus@users.noreply.github.com>
2022-07-04 13:56:02 +0300

    Add files via upload

---
+++ lesson 5/settings.py
@@ -0,0 +1,16 @@
+class Settings:
+
+    def __init__(self):
+        self.screen_color = (230, 230, 230)
+        self.screen_width = 1200
+        self.screen_height = 700
+        self.screen_caption = 'Alien Invasion'
+
+        #Ship
+        self.ship_speed = 1.5
+
+        #Bullets
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (51, 233, 246)
