ba1dff2f8f37dbef42b887680942c39d1ef4e762
donishkinus <108665225+donishkinus@users.noreply.github.com>
2022-07-21 17:10:06 +0300

    Add files via upload

---
+++ Практична/pascal 7/1.py
@@ -0,0 +1,12 @@
+"""Варіант 14"""
+
+a = int(input('Введіть а:'))
+b = int(input('Введіть b:'))
+c = int(input('Введіть c:'))
+
+a_b = bin(a)
+b_b = bin(b)
+c_b = bin(c)
+
+print('')
+print(a,'=', a_b,'|', b, '=', b_b,'|', c, '=', c_b)
